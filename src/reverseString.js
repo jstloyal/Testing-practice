357db4c1d88f7a172449a37e92ad09c595f421c4
jstloyal <jstloyalty@gmail.com>
2020-09-28 15:25:30 +0100

    Add reverseString function

---
+++ src/reverseString.js
@@ -0,0 +1,9 @@
+const reverseString = (string) => {
+  let reversedStr = "";
+  for (let i = string.length - 1; i >= 0; i -= 1) {
+    reversedStr += string[i];
+  }
+  return reversedStr;
+};
+
+module.exports = reverseString;
