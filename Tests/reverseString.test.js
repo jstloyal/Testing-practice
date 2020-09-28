481c8ceaa460db9e2b2976f9b917f781e2d2f1c1
jstloyal <jstloyalty@gmail.com>
2020-09-28 15:45:44 +0100

    Add test cases for reverseString function

---
+++ Tests/reverseString.test.js
@@ -0,0 +1,13 @@
+const reverseString = require("../src/reverseString");
+
+test("reverse single word", () => {
+  expect(reverseString("hello")).toMatch("olleh");
+});
+
+test("reverse single letter", () => {
+  expect(reverseString("a")).toMatch("a");
+});
+
+test("reverse multiple words", () => {
+  expect(reverseString("welcome home")).toMatch("emoh emoclew");
+});
